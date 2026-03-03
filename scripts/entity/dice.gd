7657fed34fabec9cda148c5bcbc6c6edc053a697
EnderSam <144076969+EnderS4m@users.noreply.github.com>
2026-03-03 08:36:41 -0800

    Figuring out signals
    I mean yeah. I have learned that signals are actually pretty hype when it comes to passing variables between different scripts

--- scripts/entity/dice.gd
+++ scripts/entity/dice.gd
@@ -1,4 +1,5 @@
 extends AnimatedSprite2D
+signal dice_result(value: int)
 
 @onready var dice = $"."
 
@@ -14,12 +15,13 @@ func _ready():
 	dice.frame = randi_range(0,5)
 	dice.play("diceRoll",1)
 	
-func _process(_delta):
+func _process(delta):
 	scale = Vector2(lerpf(1,scaleUp,progress),lerpf(1,scaleUp,progress))
 	if progress > 0.0:
-		progress -= 0.1
+		progress -= 0.1 * delta * 60
 
 func _on_frame_changed():
 	dice.frame = randi_range(0,5)
 	result = dice.frame + 1
 	progress = 1
+	dice_result.emit(result)
---
+++ scripts/entity/player_piece.gd
@@ -0,0 +1,15 @@
+extends Sprite2D
+
+var result: int = 0
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta):
+	pass
+
+func _on_dice_dice_result(value):
+	result = value
+	print(value)
