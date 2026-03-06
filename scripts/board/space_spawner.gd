0213c8086af224875b817eee13883a792d2006b2
EnderSam <144076969+EnderS4m@users.noreply.github.com>
2026-03-05 21:18:47 -0800

    On a Roll
    Starting the frame work for getting the dice to roll. Set up an enumerator for the different states the dice can be in. Simple stuff, to be earnest

--- scripts/board/space_spawner.gd
+++ scripts/board/space_spawner.gd
@@ -5,6 +5,7 @@ extends Node2D
 
 var boardLength: int = 10
 var spaceSpace: int = 54
+var spaceRise: int = 27
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -14,9 +15,10 @@ func _ready():
 		space.position = position
 		# This line isn't really THAT important, mainly just
 		# proof of concept for random space generation
-		space.self_modulate = Color(randf_range(0.5,1),randf_range(0.5,1),randf_range(0.5,1),)
+		space.self_modulate = Color(randf_range(0.5,1),randf_range(0,1),randf_range(0.5,1))
 		add_sibling.call_deferred(space)
 		position.x += spaceSpace
+		position.y += spaceRise
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
--- scripts/entity/dice.gd
+++ scripts/entity/dice.gd
@@ -2,6 +2,20 @@ extends AnimatedSprite2D
 signal dice_result(value: int)
 
 @onready var dice = $"."
+@onready var roll_timer = $RollTimer
+
+# Current state of the dice
+# IDLE: The dice slowly goes through random number results
+# ROLLING: The dice swiftly goes through random number results, slowing
+# down towards the end. Leads into the RESULT state when finished
+# RESULT: The dice is static and is showing off the result of the roll
+enum state {
+	IDLE,
+	ROLLING,
+	RESULT
+}
+# The current state of the dice, duh
+var curState
 
 # What ye be rollin'
 var result: int = 0
@@ -12,16 +26,27 @@ var progress: float = 0.0
 var scaleUp: float = 1.25
 
 func _ready():
+	curState = state.IDLE
 	dice.frame = randi_range(0,5)
-	dice.play("diceRoll",0.75)
 	
 func _process(delta):
+	result = dice.frame - 1
+	
+	if curState == state.IDLE:
+		idle(0.25)
+		
+	scaleEffect(delta)
+
+func _on_frame_changed():
+	if curState == state.IDLE or curState == state.ROLLING:
+		dice.frame = randi_range(0,5)
+		progress = 1
+	dice_result.emit(result)
+
+func scaleEffect(delta):
 	scale = Vector2(lerpf(1,scaleUp,progress),lerpf(1,scaleUp,progress))
 	if progress > 0.0:
 		progress -= 0.1 * delta * 60
 
-func _on_frame_changed():
-	dice.frame = randi_range(0,5)
-	result = dice.frame + 1
-	progress = 1
-	dice_result.emit(result)
+func idle(animSpeed: float):
+	dice.play("diceRoll",animSpeed)
